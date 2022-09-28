f80f57644aba649d35b78389000b855ec5f09d06
Prakash Chandra Palai <prakashchandrapalai91@gmail.com>
2022-09-28 07:49:40 +0530

    All commit

---
+++ src/main/java/com/prakash/java/junit/MathUtils.java
@@ -0,0 +1,30 @@
+package com.prakash.java.junit;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double computeCircleArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int addThree(int a, int b, int c) {
+        int sum = a + b + c ;
+        System.out.println("Sum is : "+sum);
+        return sum;
+    }
+}
